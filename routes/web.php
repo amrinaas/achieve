54d5b0542bc0809b0ba80fe5d8eb96c7c6c544cc
Amrina Aulia Siregar <amrinaas96@gmail.com>
2025-01-07 16:03:44 +0700

    feat: add new route

--- routes/web.php
+++ routes/web.php
@@ -1,30 +1,30 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\TaskController;
-use Illuminate\Foundation\Application;
+use App\Http\Controllers\ProjectController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
+Route::redirect('/', '/dashboard');
+
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::resource('dashboard', DashboardController::class)->except('show');
+
+    Route::resource('task', TaskController::class)->except('show');
+    Route::resource('project', ProjectController::class)->except('show');
+    Route::resource('user', UserController::class)->except('show');
 });
 
-Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
-    Route::get('/', [TaskController::class, 'index'])->name('index');
 });
 
 require __DIR__.'/auth.php';
+require __DIR__.'/api.php';
